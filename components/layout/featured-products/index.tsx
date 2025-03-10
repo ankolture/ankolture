4d77e97cbd1eb7850c6f7e4aff16f00ea6c70391
Maycol Sanchez Salazar <msanchez@serempre.com>
2025-03-10 14:09:49 -0500

    start site

---
+++ components/layout/featured-products/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import CustomCarousel from 'components/custom-carousel';
+import FeaturedCard from 'components/featured-card';
+import { Product } from 'lib/shopify/types';
+
+export default function Featuredproducts({ products }: { products: Product[] }) {
+  return (
+    <>
+      <section className="container mx-auto my-[70px] max-w-[1140px]">
+        <div className="px-2.5">
+          <h2 className="mb-2.5 text-center text-header-2 font-semibold leading-none">
+            Featured Products
+          </h2>
+          <h3 className="mx-auto max-w-3xl text-center text-header-3 leading-none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </h3>
+        </div>
+        <CustomCarousel className="mt-12 pl-2.5">
+          {products?.map((product: Product) => <FeaturedCard key={product.id} product={product} />)}
+        </CustomCarousel>
+      </section>
+    </>
+  );
+}
---
+++ components/layout/footer/index.tsx
@@ -0,0 +1,79 @@
+import { Button } from 'components/button';
+import AmericanExpress from 'public/icons/payments/americanexp';
+import Discover from 'public/icons/payments/discover';
+import Mastercard from 'public/icons/payments/mastercard';
+import Paypal from 'public/icons/payments/paypal';
+import Visa from 'public/icons/payments/visa';
+import SendNewsletterBtn from 'public/icons/sendnews';
+import Facebook from 'public/icons/social/facebook';
+import Instagram from 'public/icons/social/instagram';
+import Tiktok from 'public/icons/social/tiktok';
+import MainLogo from 'public/logo/logo';
+
+export default function Footer() {
+  const CompanyAddress = () => (
+    <div className="pt-[1.87rem] text-center sm:pt-0 sm:text-left">
+      <MainLogo width="93px" height="88px" className="mx-auto sm:mx-0" />
+      <div className="mt-2.5 leading-tight">
+        <p>12190 Beahan Street</p>
+        <p>59025</p>
+        <p>West Virginia</p>
+        <p>(979) 541-3809</p>
+        <p>info@hello.com</p>
+      </div>
+    </div>
+  );
+
+  const Newsletter = () => (
+    <div className="flex max-w-96 flex-col items-center sm:items-end sm:pt-[2rem]">
+      <h3 className="text-center text-header-3 font-semibold sm:text-right">Join our newsletter</h3>
+      <form className="relative mt-5 flex w-full max-w-72 items-center ">
+        <input
+          type="email"
+          placeholder="Your Email"
+          className="w-full rounded-full bg-[#ffefef]  py-2.5 pl-5 outline-none"
+        />
+        <Button className="absolute right-0 bg-white px-[10px]" aria-label="Newsletter">
+          <SendNewsletterBtn width="30px" height="30px" />
+        </Button>
+      </form>
+      <p className="pt-2.5 text-center leading-tight sm:text-right">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+        laudantium
+      </p>
+    </div>
+  );
+
+  const PaymentLogos = () => (
+    <div className="flex flex-wrap items-center justify-center gap-5">
+      <Visa />
+      <Mastercard />
+      <AmericanExpress />
+      <Discover />
+      <Paypal />
+    </div>
+  );
+
+  const SocialIcons = () => (
+    <div className="flex items-center gap-5 pt-[1.8rem]">
+      <Facebook className="max-w-[1.8rem]" />
+      <Instagram className="max-w-[1.8rem]" />
+      <Tiktok className="max-w-[1.8rem]" />
+    </div>
+  );
+
+  return (
+    <footer className="mt-[70px] bg-light-pink pb-10 pt-12">
+      <div className="container mx-auto flex max-w-[1140px] flex-col items-center px-2.5">
+        <div className="flex w-full flex-col-reverse items-center justify-center sm:flex-row sm:items-start sm:justify-between">
+          <CompanyAddress />
+          <Newsletter />
+        </div>
+        <div className="mt-[4.3rem] flex w-full flex-col items-center justify-center ">
+          <PaymentLogos />
+          <SocialIcons />
+        </div>
+      </div>
+    </footer>
+  );
+}
